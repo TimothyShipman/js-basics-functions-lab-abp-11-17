d95dc3c5fd10151054d6083090baf4181b27cfcc
Tim Shipman <bricklayertim@yahoo.com>
2017-11-09 18:55:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function calculateVertical(start, end){
+  let feetInBlock = 264;
+
+  let blocksTraveled = Math.abs((end - start));
+
+  return blocksTraveled *feetInBlock
+}
